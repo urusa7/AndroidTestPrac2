f9f08fb4992ca7ae678a01caea26478c02eed529
urusa7 <abt737@gmail.com>
2017-08-24 00:28:48 -0700

    fourth commit

--- app/src/main/java/com/example/urusa7/myapplication/MainActivity.java
+++ app/src/main/java/com/example/urusa7/myapplication/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //new
+        //4444444444444444
 
     }
 }
